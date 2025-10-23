e6c713da1325ccf054dab553872dc069fcd0ebbd
lg017 <laura.gutierrez.osorno@gmail.com>
2025-10-23 00:33:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,15 @@ except:
 
 st.subheader("Un fragmento de una fábula muy conocida.")
 st.write(
-         ' El hijo de rana, Rinrín renacuajo Salió esta mañana muy tieso y muy majo Con pantalón corto, corbata a la moda Sombrero encintado y chupa de boda. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+         ' El hijo de rana, Rinrín renacuajo Salió esta mañana muy tieso y muy majo,  Con pantalón corto, corbata a la moda Sombrero encintado y chupa de boda. ' 
+         ' -¡Muchacho, no salgas!- le grita mamá, pero él hace un gesto y orondo se va.'  
+         ' Halló en el camino, a un ratón vecino Y le dijo: -¡amigo!- venga usted conmigo, Visitemos juntos a doña ratona Y habrá francachela y habrá comilona.' 
+         ' A poco llegaron, y avanza ratón, Estírase el cuello, coge el aldabón,'
+        'Da dos o tres golpes, preguntan: ¿quién es? -Yo doña ratona, beso a usted los pies ¿Está usted en casa? -Sí señor sí estoy,'
+      ' y celebro mucho ver a ustedes hoy; estaba en mi oficio, hilando algodón, pero eso no importa; bienvenidos son.'
+    
+            ' -Rafael Pombo.'
+         ' -Rafael Pombo.'
         
         )
            
